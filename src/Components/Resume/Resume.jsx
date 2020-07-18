d45a4b07e5b1d66fe2d3b198c1045a781c9c15e9
Graham Miller <grahamiller96@gmail.com>
2020-07-17 21:32:11 -0400

    removing about

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -5,18 +5,9 @@ import Experience from "../General/Experience/Experience"
 const Resume = () => {
   return (
     <div className={styles.resume}>
-      <Experience
-        location={"Calgary"}
-        about={"Queen's University is one of my favourite places"}
-      />
-      <Experience
-        location={"Kingston, Queen's University"}
-        about={"Queen's University is one of my favourite places"}
-      />
-      <Experience
-        location={"Toronto"}
-        about={"Queen's University is one of my favourite places"}
-      />
+      <Experience location={"Calgary"} about={" "} />
+      <Experience location={"Kingston, Queen's University"} about={" "} />
+      <Experience location={"Toronto"} about={" "} />
     </div>
   )
 }
