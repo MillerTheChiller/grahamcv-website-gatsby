35d2cc10e9a344968ec3ca7792b783845305cc59
Graham Miller <grahamiller96@gmail.com>
2020-07-30 19:58:54 -0400

    feat: more metadata- hoping to share on linkedin

--- src/Components/Metadata/Metadata.jsx
+++ src/Components/Metadata/Metadata.jsx
@@ -1,12 +1,22 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { metadata } from "../../utils/constants"
+import shareableImage from "../../images/picture_sharing.png"
 
 const Metadata = () => {
   return (
     <Helmet>
       <meta charSet="utf-8" />
       <title> {metadata.title} </title>
+      <meta property="og:title" content={metadata.title} />
+      <meta property="og:description" content={metadata.description} />
+      <meta
+        property="og:image"
+        content={`https://grahamhenrymiller.com${shareableImage}`}
+      />
+      <meta property="og:image:width" content="1440" />
+      <meta property="og:image:height" content="700" />
+      <meta property="og:image:type" content="image/png" />
       <meta name="description" content={metadata.description} />
       <meta name="keywords" content={metadata.keywords} />
       <meta name="name" content={metadata.name} />
